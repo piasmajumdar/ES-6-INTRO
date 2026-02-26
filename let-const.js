ac913e691c0bde3a81ed80656561b3225d005cb9
piasmajumdar <piasmajumdar@gmail.com>
2026-02-26 09:54:15 +0530

    difference between var let and const

---
+++ let-const.js
@@ -0,0 +1,27 @@
+const name = 'Bangladesh';
+// name = 'Singapore'; //--> Not possible for const
+const countryName = 'BD' + name;
+console.log(countryName);
+
+
+let price = 120;
+price = 150;
+console.log(price);
+
+console.log(name22);//can access but no value
+var name22 = 'Bangladesh';
+
+
+const dress = ['Shirt', 'Pant', 'shoe'];
+// dress = [new declare] --> not possible for const
+dress.push('Belt');
+console.log(dress);
+
+const student = {
+    name: 'John',
+    age: 25
+}
+
+student.name = 'Jane';
+student.age = 30;
+console.log(student);
